80fd5a18e4d63fdde09cd3f28f0207c8172f0e42
Santiago Posada <santiago90dell@gmail.com>
2023-05-31 12:07:13 -0500

    listo el ejercicio para la practica

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Primera practica curso reactividad.");
+    }
+
+    /**
+     * Haciendo uso de lo aprendido(Expresiones lambda, referencias a métodos y funciones puras) resuelva:
+     * */
+
+    /**
+     * 1. Con la intención de encontrar el número mayor en una lista y a su vez el número menor, cree un método que le permita
+     * estas dos funciones, siguiendo los principios de las funciones puras.
+     * */
+
+    /**
+     * 2. En una tienda de ropa necesitamos aplicar un descuento a las ventas que se hacen, este descuento
+     * es variable por lo que necesitamos que usted cree un método que reciba el total de la venta y el porcentaje a descontar
+     * y devuelva el nuevo precio total.
+     * */
+
+    /**
+     * 3. Necesitamos calcular el iva de una venta, para esto es necesario que usted cree un método
+     * Tendremos dos escenarios posibles, en el primero se le pasará únicamente el valor al cual se le debe
+     * aplicar el iva, sin especificar el porcentaje, en este caso se debe aplicar un 21% siempre.
+     * En el segundo escenario se le pasará tanto el precio de la venta como el porcentaje de iva.
+     * Cree una sola función que pueda lograr estos dos propósitos
+     * */
+
+
+
+}
